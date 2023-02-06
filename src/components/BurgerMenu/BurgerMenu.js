e2e844bb07488792e93de2325734383f66eb8308
Timelas <lom.a.s@icloud.com>
2023-02-06 18:54:24 +0300

    плавное открытие бургер меню

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -6,14 +6,14 @@ function BurgerMenu(props) {
 
   return (
     <section className={`burger-menu burger-menu_${isOpenBurger && 'visible'}`}>
-      <ul className={`burger-menu__links burger-menu__links_${isOpenBurger && 'visible'}`}>
-        <li className="burger-menu__link" onClick={onCloseBurger}><a href="#home" className="burger-menu__ancor">ГЛАВНАЯ</a></li>
-        <li className="burger-menu__link" onClick={onCloseBurger}><a href="#place" className="burger-menu__ancor">ГДЕ</a></li>
-        <li className="burger-menu__link" onClick={onCloseBurger}><a href="#plan" className="burger-menu__ancor">ПЛАН ДНЯ</a></li>
-        <li className="burger-menu__link" onClick={onCloseBurger}><a href="#dresscode" className="burger-menu__ancor">ДРЕСС КОД</a></li>
-        <li className="burger-menu__link" onClick={onCloseBurger}><a href="#details" className="burger-menu__ancor">ДЕТАЛИ</a></li>
-        <li className="burger-menu__link" onClick={onCloseBurger}><a href="#rsvp" className="burger-menu__ancor">RSVP</a></li>
-        <li className="burger-menu__link" onClick={onCloseBurger}><a href="#questions" className="burger-menu__ancor">ОПРОС</a></li>
+      <ul className="burger-menu__links">
+        <li className={`burger-menu__link burger-menu__link_${isOpenBurger && 'visible'}`} onClick={onCloseBurger}><a href="#home" className="burger-menu__ancor">ГЛАВНАЯ</a></li>
+        <li className={`burger-menu__link burger-menu__link_${isOpenBurger && 'visible'}`} onClick={onCloseBurger}><a href="#place" className="burger-menu__ancor">ГДЕ</a></li>
+        <li className={`burger-menu__link burger-menu__link_${isOpenBurger && 'visible'}`} onClick={onCloseBurger}><a href="#plan" className="burger-menu__ancor">ПЛАН ДНЯ</a></li>
+        <li className={`burger-menu__link burger-menu__link_${isOpenBurger && 'visible'}`} onClick={onCloseBurger}><a href="#dresscode" className="burger-menu__ancor">ДРЕСС КОД</a></li>
+        <li className={`burger-menu__link burger-menu__link_${isOpenBurger && 'visible'}`} onClick={onCloseBurger}><a href="#details" className="burger-menu__ancor">ДЕТАЛИ</a></li>
+        <li className={`burger-menu__link burger-menu__link_${isOpenBurger && 'visible'}`} onClick={onCloseBurger}><a href="#rsvp" className="burger-menu__ancor">RSVP</a></li>
+        <li className={`burger-menu__link burger-menu__link_${isOpenBurger && 'visible'}`} onClick={onCloseBurger}><a href="#questions" className="burger-menu__ancor">ОПРОС</a></li>
       </ul>
       <button className="burger-menu__close" onClick={onCloseBurger}>Закрыть</button>
     </section>
