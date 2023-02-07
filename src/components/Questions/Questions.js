98b9d22bfe620a635644604085b12585505db7dd
Timelas <lom.a.s@icloud.com>
2023-02-07 14:21:28 +0300

    отправка формы

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,54 +1,80 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 import Title from "../Title/Title";
 import "./Questions.css";
 
 function Questions() {
+  const formRef = useRef(null)
+  const scriptUrl = "https://script.google.com/macros/s/AKfycbwJxP4aDFSrU_RjHAlVO_ea08w8XqHFhTSF3eg90ANlk9eRo2gupGzYnmt5TKrBaN3H8Q/exec"
+  const [loading, setLoading] = useState(false);
+  const [isFormVisible, setisFormVisible] = useState(false);
+
+  const handleSubmit = (e) =>{
+    console.log(formRef.current)
+      e.preventDefault()
+      setLoading(true)
+
+      fetch(scriptUrl, {
+      method: 'POST',
+      body: new FormData(formRef.current),
+
+  }).then(res => {
+          setLoading(false)
+      })
+      .catch(err => console.log(err))
+  }
+  
   return (
     <section className="question" id="questions">
       <Title title={"Опрос"}/>
-      <p className="question__text">Чтобы все прошло идеально и этот день запомнился надолго, пожалуйста,
-        ответьте на несколько вопросов
-      </p>
-      <form className="question__form">
-        <p className="question__string">
-          <label htmlFor="who" className="question__label-input">ИМЯ И ФАМИЛИЯ:</label>
-          <input type="text" name="who" id="who" required className="question__input"></input>
-        </p>
-        <fieldset className="question__string">Понадобится ли вам трансфер:
-          <label  htmlFor="yes-transfer" className="question__label">
-            <input className="question__unvisible" type="radio" name="transfer" value="yes-transfer" id="yes-transfer"></input>
-            <span className="question__visible-radio"></span>
-            <span className="question__text-span">Да</span>
-          </label>
-          <label className="question__label" htmlFor="no-transfer">
-            <input className="question__unvisible" type="radio" name="transfer" value="no-transfer" id="no-transfer"></input>
-            <span className="question__visible-radio"></span>
-            <span className="question__text-span">Нет</span>
-          </label>
-        </fieldset>
-        <fieldset className="question__string">Предпочтения по еде:
-          <label  htmlFor="chicken" className="question__label">
-            <input className="question__unvisible" type="checkbox" name="eat" value="chicken" id="chicken"></input>
-            <span className="question__visible-checkbox"></span>
-            <span className="question__text-span">Курица</span>
-          </label>
-          <label htmlFor="fish" className="question__label">
-            <input className="question__unvisible" type="checkbox" name="eat" value="fish" id="fish"></input>
-            <span className="question__visible-checkbox"></span>
-            <span className="question__text-span">Рыба</span>
-          </label>
-          <label htmlFor="meat" className="question__label">
-            <input className="question__unvisible" type="checkbox" name="eat" value="meat" id="meat"></input>
-            <span className="question__visible-checkbox"></span>
-            <span className="question__text-span">Мясо</span>
-          </label>
-        </fieldset>
-        <p className="question__string">
-          <label htmlFor="alco" className="question__label-input">Пожелания по алкоголю:</label>
-          <input type="text" name="alco" id="alco" required className="question__input"></input>
-        </p>
-        <button type="submit" className="question__button">Подтвердить</button>
-      </form>
+
+      { isFormVisible ?
+        <div className="question__container">
+          <p className="question__text">Чтобы все прошло идеально и этот день запомнился надолго, пожалуйста, ответьте на несколько вопросов</p>
+          <form className="question__form" ref={formRef} onSubmit={handleSubmit}>
+          <p className="question__string">
+            <label htmlFor="who" className="question__label-input">ИМЯ И ФАМИЛИЯ:</label>
+            <input type="text" name="Имя" id="who" required className="question__input" minLength={2}></input>
+          </p>
+          <fieldset className="question__string">Понадобится ли вам трансфер:
+            <label  htmlFor="yes-transfer" className="question__label">
+              <input className="question__unvisible" type="radio" name="Трансфер" value="Нужен" id="yes-transfer"></input>
+              <span className="question__visible-radio"></span>
+              <span className="question__text-span">Да</span>
+            </label>
+            <label className="question__label" htmlFor="no-transfer">
+              <input className="question__unvisible" type="radio" name="Трансфер" value="Нужен" id="no-transfer"></input>
+              <span className="question__visible-radio"></span>
+              <span className="question__text-span">Нет</span>
+            </label>
+          </fieldset>
+          <fieldset className="question__string">Предпочтения по еде:
+            <label  htmlFor="chicken" className="question__label">
+              <input className="question__unvisible" type="checkbox" name="Еда" value="Курица" id="chicken"></input>
+              <span className="question__visible-checkbox"></span>
+              <span className="question__text-span">Курица</span>
+            </label>
+            <label htmlFor="fish" className="question__label">
+              <input className="question__unvisible" type="checkbox" name="Еда" value="Рыба" id="fish"></input>
+              <span className="question__visible-checkbox"></span>
+              <span className="question__text-span">Рыба</span>
+            </label>
+            <label htmlFor="meat" className="question__label">
+              <input className="question__unvisible" type="checkbox" name="Еда" value="Мясо" id="meat"></input>
+              <span className="question__visible-checkbox"></span>
+              <span className="question__text-span">Мясо</span>
+            </label>
+          </fieldset>
+          <p className="question__string">
+            <label htmlFor="alco" className="question__label-input">Пожелания по алкоголю:</label>
+            <input type="text" name="Алкоголь" id="alco" required className="question__input"></input>
+          </p>
+          <button type="submit" className="question__button">{loading ? "Отправка..." : "Отправить"}</button>
+        </form> 
+      </div>
+      :
+      <p className="question__text">Форма отправлена! До встречи!</p>
+      }
+      
     </section>
   );
 }
--- src/components/RSVP/Rsvp.js
+++ src/components/RSVP/Rsvp.js
@@ -1,28 +1,61 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 import Title from "../Title/Title";
 import "./Rsvp.css";
 
 function Rsvp() {
+  const formRef = useRef(null)
+  const scriptUrl = "https://script.google.com/macros/s/AKfycbwJxP4aDFSrU_RjHAlVO_ea08w8XqHFhTSF3eg90ANlk9eRo2gupGzYnmt5TKrBaN3H8Q/exec"
+  const [loading, setLoading] = useState(false);
+  const [isFormVisible, setisFormVisible] = useState(true);
+
+  const handleSubmit = (e) =>{
+    console.log(formRef.current)
+      e.preventDefault()
+      setLoading(true)
+
+      fetch(scriptUrl, {
+      method: 'POST',
+      body: new FormData(formRef.current),
+
+  }).then(res => {
+          setLoading(false);
+          setisFormVisible(false);
+      })
+      .catch(err => console.log(err))
+  }
+
+  function returnForm () {
+    setisFormVisible(true);
+  }
+
   return (
     <section className="rsvp" id="rsvp">
       <Title title={"RSVP"}/>
-      <p className="rsvp__text">Пожалуйста, подтвердите ваше присутствие на нашем празднике до
-      01 июня 2023 года любым удобным для вас способом или заполните форму ниже:
-      </p>
-      <form className="rsvp__form">
-        <p className="rsvp__string">
-          <label htmlFor="name" className="rsvp__label">Фамилия и имя</label>
-          <input type="text" name="name" id="name" required className="rsvp__input"></input>
+      { isFormVisible ?
+      <div className="rsvp__container">
+        <p className="rsvp__text">Пожалуйста, подтвердите ваше присутствие на нашем празднике до
+        01 июня 2023 года любым удобным для вас способом или заполните форму ниже:
         </p>
-        <p className="rsvp__string">
-          <label htmlFor="person" className="rsvp__label">количество персон</label>
-          <input type="number" min="0" max="10" name="person" id="person" required className="rsvp__input"></input>
-        </p>
-        <div className="rsvp__buttons">
-        <button type="submit" className="rsvp__button">подтвердить</button>
-        <button type="submit" className="rsvp__button">отклонить</button>
-        </div>
-      </form>
+        <form className="rsvp__form" ref={formRef} onSubmit={handleSubmit}>
+          <p className="rsvp__string">
+            <label htmlFor="name" className="rsvp__label">Фамилия и имя</label>
+            <input type="text" name="ФИО" id="name" required className="rsvp__input"></input>
+          </p>
+          <p className="rsvp__string">
+            <label htmlFor="person" className="rsvp__label">количество персон</label>
+            <input type="number" min="0" max="10" name="Количество" id="person" required className="rsvp__input"></input>
+          </p>
+          <div className="rsvp__buttons">
+          <button className="rsvp__button" name="Принято">{loading ? "Отправка..." : "Подтвердить"}</button>
+          <button className="rsvp__button" name="Отклонено">{loading ? "Отправка..." : "Отклонить"}</button>
+          </div>
+        </form>
+      </div> :
+      <div>
+        <p className="question__answer">Форма отправлена!</p>
+        <button className="question__return" onClick={returnForm}>Планы поменялись? Отправить заново</button>
+      </div>
+      }
     </section>
   );
 }
